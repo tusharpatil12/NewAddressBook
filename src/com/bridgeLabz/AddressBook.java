263a41a8b099774f969dd4673fbf0ab97a51d098
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 <tusharpatil22700@gmail.com>
2022-11-08 08:41:24 -0800

    welcome

---
+++ src/com/bridgeLabz/AddressBook.java
@@ -0,0 +1,12 @@
+package com.bridgeLabz;
+
+import java.util.Scanner;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to Address Book Program new");
+
+	}
+}
